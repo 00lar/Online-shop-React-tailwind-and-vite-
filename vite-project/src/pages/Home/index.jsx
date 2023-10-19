b8c1e2724592cf0b31f8dafbf82654dda8e9ff55
00lar <eduardani84@gmail.com>
2023-10-18 22:42:24 -0600

    consumiendo FakeStore API

--- vite-project/src/pages/Home/index.jsx
+++ vite-project/src/pages/Home/index.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react"
 import { Layout } from "../../components/Layout"
 import { Card } from "../../components/card"
+import { useEffect } from "react";
+import { apiurl } from "../../api";
 
 function Home() {
+  const [items,setItems] = useState(null);
+  useEffect(()=> {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${apiurl}/products`)
+        const data = await res.json()
+        setItems(data)
+      } catch (error) {
+        console.log(error)
+      }
+    } 
+    fetchData()
+  },[])
+
+
+
+  
     return (
       <Layout>
         Home
-        <Card/>
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item=> (
+            <Card
+            key={item.id}
+            data={item}
+            />
+          ))
+        }
+        </div>
       </Layout>
     )
   }
