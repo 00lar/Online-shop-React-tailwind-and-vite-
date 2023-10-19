c7330a82a7599abc346b7b618a366e9a35de986f
00lar <eduardani84@gmail.com>
2023-10-19 15:21:50 -0600

    Layout Product Detail

--- vite-project/src/Context/index.jsx
+++ vite-project/src/Context/index.jsx
@@ -5,11 +5,17 @@ const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({children}) => {
     const [count,setCount] = useState(0)
+    const [isProductDetailOpen,setIsProductDetailOpen] = useState(false)
+    const OpenProductDetail = () => setIsProductDetailOpen(true)
+    const CloseProductDetail = () => setIsProductDetailOpen(false)
     return (
         <ShoppingCartContext.Provider
         value={{
             count,
             setCount,
+            OpenProductDetail,
+            CloseProductDetail,
+            isProductDetailOpen
         }}
         >
             {children}
--- vite-project/src/components/card/index.jsx
+++ vite-project/src/components/card/index.jsx
@@ -5,7 +5,9 @@ import { AddIcon } from "../Icons"
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
     return(
-        <div className="bg-white cursor-pointer w-56 h-60">
+        <div 
+            onClick={()=> context.OpenProductDetail()}
+            className="bg-white cursor-pointer w-56 h-60">
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-xl text-black text-xs m-2 px-3 py-0.6">{data.data.category.name}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt="HeadPhone"></img>
