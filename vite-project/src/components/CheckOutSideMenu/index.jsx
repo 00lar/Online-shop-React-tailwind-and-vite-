a5ddb7275526e1a45d8752096296cb3dfa4f9c19
00lar <eduardani84@gmail.com>
2023-10-22 23:25:05 -0600

    Scrollabe bar added

--- vite-project/src/components/CheckOutSideMenu/index.jsx
+++ vite-project/src/components/CheckOutSideMenu/index.jsx
@@ -2,12 +2,13 @@ import { Xicon } from "../Icons"
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context"
 import { OrderCard } from "../OrderCard"
+import "./styles.css"
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
 return (
     <aside 
-        className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-60px)] top-20 flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+        className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} scrollable-cards w-[360px] h-[calc(100vh-60px)] top-20 flex-col fixed right-0 border border-black rounded-lg bg-white `}>
         <div className="flex justify-between items-center p-6">
             <h2 className=" font-medium">My Order</h2>
             <div
